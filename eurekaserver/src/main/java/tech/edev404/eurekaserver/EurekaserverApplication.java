08d5825ae9cb776458986e7d191271b822089bd2
Eduardo Ramirez <eduardoramirezdev@outlook.com>
2023-05-02 15:32:06 -0500

    EnableEurekaServer annotation added

--- eurekaserver/src/main/java/tech/edev404/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/tech/edev404/eurekaserver/EurekaserverApplication.java
@@ -2,7 +2,11 @@ package tech.edev404.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@RefreshScope
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaserverApplication {
 
