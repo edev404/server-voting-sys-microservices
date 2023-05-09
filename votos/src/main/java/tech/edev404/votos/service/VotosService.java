47ffe4e94cd40cf1a4d453bffa80b3a455b097e6
Eduardo Ramirez <eduardoramirezdev@outlook.com>
2023-05-08 19:21:23 -0500

    Updating votosService

--- votos/src/main/java/tech/edev404/votos/service/VotosService.java
+++ votos/src/main/java/tech/edev404/votos/service/VotosService.java
@@ -1,12 +1,7 @@
 package tech.edev404.votos.service;
 
-import tech.edev404.votos.model.Aprendiz;
-import tech.edev404.votos.model.Candidato;
-import tech.edev404.votos.model.Votacion;
-
 public interface VotosService {
 
-    void registerVote(Candidato candidato, Aprendiz aprendiz, Votacion votacion);
     Boolean hasAlreadyVote(String idAprendiz, Integer idVotacion);
     void registerVote(Integer idCandidato, String idAprendiz, Integer idVotacion);
     
--- votos/src/main/java/tech/edev404/votos/service/implementation/VotosServiceImpl.java
+++ votos/src/main/java/tech/edev404/votos/service/implementation/VotosServiceImpl.java
@@ -2,9 +2,6 @@ package tech.edev404.votos.service.implementation;
 
 import org.springframework.stereotype.Service;
 
-import tech.edev404.votos.model.Aprendiz;
-import tech.edev404.votos.model.Candidato;
-import tech.edev404.votos.model.Votacion;
 import tech.edev404.votos.service.VotosService;
 
 @Service
@@ -20,10 +17,6 @@ public class VotosServiceImpl implements VotosService {
         
     }
 
-    @Override
-    public void registerVote(Candidato candidato, Aprendiz aprendiz, Votacion votacion) {
-        
-    }
 
     
 }
