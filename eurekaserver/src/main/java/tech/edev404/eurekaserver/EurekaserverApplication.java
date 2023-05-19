992253f1f7533de0c4000d47e10e311a8b71d5b6
Eduardo Ramirez <eduardoramirezdev@outlook.com>
2023-05-19 16:50:39 -0500

    Gateway server added

--- eurekaserver/src/main/java/tech/edev404/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/tech/edev404/eurekaserver/EurekaserverApplication.java
@@ -10,8 +10,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaserverApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaserverApplication.class, args);
+  }
 
 }
--- votaciones/src/main/java/tech/fabricatic/votaciones/controller/VotosController.java
+++ votaciones/src/main/java/tech/fabricatic/votaciones/controller/VotosController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import tech.fabricatic.votaciones.service.CandidatosService;
@@ -24,37 +25,38 @@ import tech.fabricatic.votaciones.service.feign.IdentidadFeignClient;
 @SuppressWarnings("unused")
 public class VotosController {
 
-    private final VotosService votosService;
-    private final CandidatosService candidatosService;
-    private final VotacionesService votacionesService;
-    private final IdentidadFeignClient identidadFeignClient;
+  private final VotosService votosService;
+  private final CandidatosService candidatosService;
+  private final VotacionesService votacionesService;
+  private final IdentidadFeignClient identidadFeignClient;
 
-    @PostMapping("{idAprendiz}/vote-by/{idCandidato}")
-    public ResponseEntity<HttpStatus> handleRegisterVoto(
-            @PathVariable("idCandidato") Integer idCandidato,
-            @PathVariable("idUsuario") String idUsuario,
-            @RequestParam("idVotacion") Integer idVotacion) throws EntityNotFoundException {
+  @PostMapping("{idAprendiz}/vote-by/{idCandidato}")
+  public ResponseEntity<HttpStatus> handleRegisterVoto(
+      @PathVariable("idCandidato") Integer idCandidato,
+      @PathVariable("idUsuario") String idUsuario,
+      @RequestParam("idVotacion") Integer idVotacion) throws EntityNotFoundException {
 
-        votosService.registerVote(idCandidato, idUsuario, idVotacion);
-        return ResponseEntity.status(HttpStatus.OK).build();
+    votosService.registerVote(idCandidato, idUsuario, idVotacion);
+    return ResponseEntity.status(HttpStatus.OK).build();
 
-    }
+  }
 
-    @GetMapping("{idUsuario}/has-already-vote/{idVotacion}")
-    public ResponseEntity<Boolean> handleHasAlreadyVote(
-            @PathVariable String idUsuario,
-            @PathVariable Integer idVotacion) {
-        return ResponseEntity.status(HttpStatus.OK).body(votosService.hasAlreadyVote(idUsuario, idVotacion));
-    }
+  @GetMapping("{idUsuario}/has-already-vote/{idVotacion}")
+  public ResponseEntity<Boolean> handleHasAlreadyVote(
+      @PathVariable String idUsuario,
+      @PathVariable Integer idVotacion) {
+    return ResponseEntity.status(HttpStatus.OK).body(votosService.hasAlreadyVote(idUsuario, idVotacion));
+  }
 
-    @GetMapping("feign-working")
-    public String feignWorking(){
-        return identidadFeignClient.feignWorking();
-    }
+  @CircuitBreaker(name = "usuariosfeignReport")
+  @GetMapping("feign-working")
+  public String feignWorking() {
+    return identidadFeignClient.feignWorking();
+  }
 
-    @GetMapping("hello-world")
-    public String helloWorld(){
-        return "hello from votaciones microservice";
-    }
+  @GetMapping("hello-world")
+  public String helloWorld() {
+    return "hello from votaciones microservice";
+  }
 
 }
