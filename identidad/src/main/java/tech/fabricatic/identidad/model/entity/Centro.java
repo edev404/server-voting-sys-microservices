ffcd8720d0b0c39710cd55e0bf90dae0bfe32c36
Eduardo Ramirez <eduardoramirezdev@outlook.com>
2023-05-19 22:27:25 -0500

    Update Centro.java

--- identidad/src/main/java/tech/fabricatic/identidad/model/entity/Centro.java
+++ identidad/src/main/java/tech/fabricatic/identidad/model/entity/Centro.java
@@ -5,7 +5,6 @@ import java.util.List;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Index;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
