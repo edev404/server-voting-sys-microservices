fc2040ad5e4b54e23c7c1fc08bb6319ce8bc37bd
Eduardo Ramirez <eduardoramirezdev@outlook.com>
2023-05-19 23:13:10 -0500

    Update GatewayserverApplication.java

--- gatewayserver/src/main/java/tech/fabricatic/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/tech/fabricatic/gatewayserver/GatewayserverApplication.java
@@ -2,7 +2,11 @@ package tech.fabricatic.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
+@RefreshScope
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayserverApplication {
 
